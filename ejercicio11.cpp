21b16e56ca553689dd578bfb630c38afba9a8748
Alexander-Atonal-Lara <168613131+Alexander-Atonal-Lara@users.noreply.github.com>
2024-06-05 12:23:03 -0600

    Add files via upload
    Ejercicio 11

---
+++ ejercicio11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int hora;
+ cout<<"Que hora es??"<<endl;
+ cin>>hora;
+ if(hora<12)
+ {
+  cout<<"Buenos dias"<<endl;........
+ }
+  if(hora>=12)
+ {
+      if(hora<20)
+ {
+  cout<<"Buenas tardes"<<endl;
+ }
+ }
+  if(hora>=20)
+ {
+  cout<<"Buenas noches"<<endl;
+ }
+
+ 
+}
