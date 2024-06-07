d3729273ae7c37246add4bc3576e7523254896e5
Alexander Atonal Lara <alexbirds23@gmail.com>
2024-06-06 20:22:17 -0500

    ejercicio 25

---
+++ ejercicio25.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e,f,g,h,i,j, mayor;
+	cout<<"Ingresa un numero:"<<endl;
+	cin>>a;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>b;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>c;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>d;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>e;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>f;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>g;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>h;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>i;
+    cout<<"Ingresa un numero:"<<endl;
+	cin>>j;
+    mayor=a;
+    if (mayor<b)
+    {
+        mayor=b;
+    }
+    if (mayor<c)
+    {
+        mayor=c;
+    }
+     if (mayor<d)
+    {
+        mayor=d;
+    }
+    if (mayor<e)
+    {
+        mayor=e;
+    }
+     if (mayor<f)
+    {
+        mayor=f;
+    }
+    if (mayor<g)
+    {
+        mayor=g;
+    }
+     if (mayor<h)
+    {
+        mayor=h;
+    }
+    if (mayor<i)
+    {
+        mayor=i;
+    }
+     if (mayor<j)
+    {
+        mayor=j;
+    }
+    cout<<"tu numero mayor es "<<mayor<<endl;
+return 0;
+
+
+}
