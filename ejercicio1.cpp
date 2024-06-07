d1d491d1e28df573c565e3691c9d1a23a02d8592
Alexander Atonal Lara <alexbirds23@gmail.com>
2024-06-06 20:08:54 -0500

    ejercicio 1

---
+++ ejercicio1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int lol,olo,ol;
+    cout<<"Bienvenido al sistema, igrese sus 3 numeros:"<<endl;//contraseña 1, 2, 3.
+	cout<<"Ingresa un numero:"<<endl;
+	cin>>lol;
+	cout<<"Ingresa un numero:"<<endl;
+	cin>>olo;
+	cout<<"Ingresa un numero:"<<endl;
+    cin>>ol;
+    if (lol==1)
+    {
+    	 if (olo==2)
+    {
+    	if (ol==3)
+    {
+    printf("Acceso permitido");
+    } else {
+        cout<<"error"<<endl;
+    }
+    } else {
+        cout<<"error"<<endl;
+    }
+    } else {
+        cout<<"error"<<endl;
+    }
+return 0;	
+
+}
