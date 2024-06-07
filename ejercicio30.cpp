9523ed4a3b8909ab8a0f59f86999cacaa39e6048
Alexander Atonal Lara <alexbirds23@gmail.com>
2024-06-06 20:23:39 -0500

    ejercicio 30

---
+++ ejercicio30.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string usuario;
+    string gg;
+    string num;
+    int intentos = 3;
+    string numlol = "2b2";
+    string gglol = "Alexander";
+    int precio,piezas, lol, olo=0;
+    piezas=1;
+    int i;
+
+    while (intentos > 0) {
+        cout << "Ingrese su usuario" << endl;
+        cin >> gg;
+        cout << "Ingrese su contraseña" << endl;
+        cin >> num;
+
+        if (gg == gglol && num == numlol) {
+            cout << "Bienvenido al sistema";
+         for (i=1;piezas>0;i++){
+         cout<<"¿Cuantas piezas quieres llevar?"<<endl;
+         cin >> piezas;
+         if(piezas>0){
+         cout << "¿Cual es el precio de la pieza?"<<endl;
+         cin >> precio;
+         lol=piezas*precio;
+         olo=olo+lol;}
+    }
+         cout <<"El total de tu factura es de:"<< olo << endl;
+            intentos = 0;
+        } else {
+            intentos--;
+            if (intentos > 0) {
+                cout << "Error, te quedan " << intentos << " intentos" << endl;
+            } else {
+                cout << "Has agotado tus intentos" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
