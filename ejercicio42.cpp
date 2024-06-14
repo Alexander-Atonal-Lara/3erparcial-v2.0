d506d5122d9da5d3eede8335dcdef82ad097b90b
Alexander Atonal Lara <alexbirds23@gmail.com>
2024-06-13 22:55:39 -0500

    correcion del ejercicio 42

--- ejercicio42.cpp
+++ ejercicio42.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main() {
-    int acum = 0;
     int f, c, i, j, numero;
 
     cout << "Digite el numero de filas: ";
@@ -11,16 +10,13 @@ int main() {
     cin >> c;
 
     int matriz[f][c];
-
     for (i = 0; i < f; i++) {
         for (j = 0; j < c; j++) {
             cout << "Digite dato para la fila " << i + 1 << " columna " << j + 1 << ": ";
             cin >> numero;
             matriz[i][j] = numero;
         }
-        acum += matriz[i][0];
     }
-
     cout << "Los datos que hay en la matriz son:" << endl;
     for (i = 0; i < f; i++) {
         for (j = 0; j < c; j++) {
@@ -28,8 +24,6 @@ int main() {
         }
         cout << endl;
     }
-
-    cout << "los datos almacenados en la matriz sons: " << acum << endl;
 
     return 0;
 }
